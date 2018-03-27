5ec698e35e3df54a332bbc9b188a6e6fb11511c9
Anthony Tournier <anthony.tournier25@gmail.com>
2018-03-27 11:13:35 +0200

    Exercice 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@
  * CSS d'un élement
  */
 function getStyleValue(element, property) {
-    return window.getComputedStyle(element, null).getPropertyValue(property);
+    return parseFloat(window.getComputedStyle(element, null).getPropertyValue(property));
 }
  
 
@@ -20,16 +20,88 @@ function getStyleValue(element, property) {
 // * La taille par défaut (au chargement) du text (parseFloat permet d'enlever le "px" retourné par la fonction getStyleValue)
 var btnBigger = document.getElementById("setBiggerText");
 var biggerText = document.getElementById("biggerText");
-var defaultTextSize = parseFloat(getStyleValue(biggerText, 'font-size'));
+var defaultTextSize = (getStyleValue(biggerText, 'font-size'));
 
 // Au click du bouton
 btnBigger.addEventListener("click", function() {
     // Si la taille par défaut est différente de la taille actuelle du texte
-    if(parseFloat(getStyleValue(biggerText, 'font-size')) != defaultTextSize) {
+    if(getStyleValue(biggerText, 'font-size') != defaultTextSize) {
         // alors on remet la taille par défaut
         biggerText.style.fontSize = defaultTextSize + "px";
+        // on change le contenu du bouton
+        btnBigger.textContent = "Agrandir le texte";
     } else {
         // sinon, on ajoute 10px à la taille par défaut
         biggerText.style.fontSize = (defaultTextSize + 10) + "px";
+        // on change le contenu du bouton
+        btnBigger.textContent = "Réduire le texte";
     }
 });
+
+
+
+
+
+
+/**
+ * Exercice 2
+ * @desc Créer 5 boutons et un rectangle bleu de 100px par 100px au centre de 
+ * la page. Chaque bouton provoque une action sur le rectangle.
+ * Bouton 1 : augmente la hauteur de 10px, si il dépasse 300px, il remet la hauteur à 100px
+ * Bouton 2 : met le rectangle en vert
+ * Bouton 3 : remet les couleurs initiales
+ * Bouton 4 : fait disparaître le rectangle
+ * Bouton 5 : fait réapparaître le rectangle 
+ */
+
+var rectangle = document.getElementById('rectangle');
+var btnBiggerRect = document.getElementById('setBiggerRect');
+var btnGreenRect = document.getElementById('setGreenRect');
+var btnResetColorsRect = document.getElementById('resetColorsRect');
+var btnHideRect = document.getElementById('hideRect');
+var btnShowRect = document.getElementById('showRect');
+
+// au clic du bouton pour agrandir le rectangle
+btnBiggerRect.addEventListener("click", function() {
+    // on récupère sa hauteur, que l'on stocke dans uen variable
+    var rectangleHeight = getStyleValue(rectangle, 'height');
+
+    // si cette hauteur est inférieur à 300
+    if (rectangleHeight < 300) {
+        // on l'incrémente de 10px
+        rectangle.style.height = (rectangleHeight + 10) + "px";
+    } else {
+        // sinon, il faut la remette à 100px
+        rectangle.style.height = "100px";
+    }
+
+    // petit trip, afficher la hauteur du rectangle dans ce dernier
+    rectangle.innerHTML = "<span>" + getStyleValue(rectangle, 'height') + "px </span>";
+});
+
+// au clic sur le bouton pour mettre en vert le rectangle
+btnGreenRect.addEventListener("click", function() {
+    // alors on défini le background color de rectangle
+    rectangle.style.backgroundColor = "#28a745";
+}); 
+
+// au clic sur le bouton pour remettre à zero les couleurs du rectangle
+btnResetColorsRect.addEventListener("click", function() {
+    // alors on défini le background color de rectangle
+    rectangle.style.backgroundColor = "#007bff";
+}); 
+
+// au clic sur le bouton pour cacher le rectangle
+btnHideRect.addEventListener("click", function() {
+    // alors on défini la propriété CSS display à none
+    rectangle.style.display = "none";
+}); 
+
+// au clic sur le bouton pour afficher le rectangle
+btnShowRect.addEventListener("click", function() {
+    // alors on défini la propriété CSS display à none
+    rectangle.style.display = "flex"; 
+    // Attention, j'ai mis "flex" car j'ai utilisé les flexbox pour son positionnement
+    // vous pouvez tout à fait utiliser "block" par exemple 
+}); 
+
