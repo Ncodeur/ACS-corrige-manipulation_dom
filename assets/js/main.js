d5c27f0a9439a404d3e2564a256a55471a8e5b88
Anthony Tournier <anthony.tournier25@gmail.com>
2018-03-27 11:30:39 +0200

    Exercice 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 /**
  * Functions
- * @desc Cette fonction nous permets de nous retourner la valeur d'une propriété
+ * @description Cette fonction nous permets de nous retourner la valeur d'une propriété
  * CSS d'un élement
  */
 function getStyleValue(element, property) {
@@ -45,7 +45,7 @@ btnBigger.addEventListener("click", function() {
 
 /**
  * Exercice 2
- * @desc Créer 5 boutons et un rectangle bleu de 100px par 100px au centre de 
+ * @description Créer 5 boutons et un rectangle bleu de 100px par 100px au centre de 
  * la page. Chaque bouton provoque une action sur le rectangle.
  * Bouton 1 : augmente la hauteur de 10px, si il dépasse 300px, il remet la hauteur à 100px
  * Bouton 2 : met le rectangle en vert
@@ -93,15 +93,41 @@ btnResetColorsRect.addEventListener("click", function() {
 
 // au clic sur le bouton pour cacher le rectangle
 btnHideRect.addEventListener("click", function() {
-    // alors on défini la propriété CSS display à none
-    rectangle.style.display = "none";
+    // alors on défini la propriété CSS visibility à hidden
+    rectangle.style.visibility = "hidden";
 }); 
 
 // au clic sur le bouton pour afficher le rectangle
 btnShowRect.addEventListener("click", function() {
-    // alors on défini la propriété CSS display à none
-    rectangle.style.display = "flex"; 
-    // Attention, j'ai mis "flex" car j'ai utilisé les flexbox pour son positionnement
-    // vous pouvez tout à fait utiliser "block" par exemple 
+    // alors on défini la propriété CSS visibility à visible
+    rectangle.style.visibility = "visible"; 
 }); 
 
+
+
+
+/**
+ * Exercice 3
+ * @description Reprendre le rectangle bleu, et lorsqu’on appui sur la flèche du
+ * haut le faire monter de 100px puis reprendre sa place initiale. 
+ * ATTENTION il doit monter et descendre en un appui uniquement
+ */
+
+// à l'appui d'une touche du clavier
+document.addEventListener("keydown", function(event) {
+    // event.keyCode nous permet de nous retourner l'identifiant de la touche
+    // enfoncée. 38 = flèche du haut
+    if(event.keyCode == 38) {
+        rectangle.style.top = "10px";
+    }
+});
+
+
+// au relachement d'une touche du clavier
+document.addEventListener("keyup", function(event) {
+    // event.keyCode nous permet de nous retourner l'identifiant de la touche
+    // enfoncée. 38 = flèche du haut
+    if(event.keyCode == 38) {
+        rectangle.style.top = "0px";
+    }
+});
